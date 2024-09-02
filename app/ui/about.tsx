40c92b4052576cd91d6333de457c268464f158c0
alejandro77martinez <neil.martinez@estudiante.uacm.edu.mx>
2024-09-02 00:52:12 -0600

    Pagina about responsive al 100% aun pendiente informacion a mostrar

--- app/ui/about.tsx
+++ app/ui/about.tsx
@@ -8,20 +8,18 @@ function Bar({percent}: {percent:number}){
 
 export default function AboutMe() {
   return (
-    <div className="flex flex-col px-10">
-      <div className="flex">
-        <div className="w-2/4">
-          <div className="h-28 flex justify-center items-center">
+    <div className="flex flex-col max-w-7xl mx-auto">
+      <div className="flex max-lg:flex-col">
+        <div className="w-2/4 max-lg:w-full">
+          <div className="lg:h-28 flex justify-center lg:items-center pb-7">
             <h1 className="text-3xl font-bold underline decoration-wavy">Sobre mi</h1>
           </div>
-          <div className="h-auto">
-            <div className="bg-violet-200 w-5/6 p-4 shadow-2xl shadow-black rounded-xl border-2 border-black mx-auto">
+          <div className="h-auto w-5/6 mx-auto max-lg:w-full max-lg:px-4">
+            <div className="bg-violet-200 p-4 shadow-2xl shadow-black rounded-xl border-2 border-black">
               <h2 className="text-xl font-semibold text-center">Formacion academica</h2>
               <br></br>
               <ul className="list-disc list-inside">
-                <li>
-                  Pasante en Ingenieria de Software - Universidad Autonoma de la Ciudad de Mexico (ultimo año)
-                </li>
+                <li>Licenciatura en Ingenieria de Software (ultimo año) - Universidad Autonoma de la Ciudad de Mexico</li>
                 <li>Curso sobre Web Resposive - FreeCodeCamp</li>
                 <li>Curso sobre librerias de front-end - FreeCodeCamp</li>
                 <li>Curso de SQL basico - SelfLearnign</li>
@@ -31,9 +29,9 @@ export default function AboutMe() {
             </div>
           </div>
         </div>
-        <div className="w-2/4 pt-10">
-          <div className="h-auto">
-            <div className="bg-violet-200 w-5/6 p-4 shadow-2xl shadow-black rounded-xl border-2 border-black mx-auto">
+        <div className="w-2/4 max-lg:w-full pt-10">
+          <div className="h-auto w-5/6 mx-auto max-lg:w-full max-lg:px-4">
+            <div className="bg-violet-200 p-4 shadow-2xl shadow-black rounded-xl border-2 border-black">
               <h2 className="text-xl font-semibold text-center">Habilidades tecnicas</h2>
               <br></br>
               <table className="w-full">
@@ -76,8 +74,8 @@ export default function AboutMe() {
           </div>
         </div>
       </div>
-      <div className="h-auto my-20 mx-auto">
-        <div className="mx-auto bg-violet-200 w-5/6 p-4 shadow-2xl shadow-black rounded-xl border-2 border-black">
+      <div className="h-auto py-10 lg:pt-16 max-lg:w-full max-lg:px-4 w-2/4 mx-auto">
+        <div className="mx-auto bg-violet-200 p-4 lg:w-5/6 shadow-2xl shadow-black rounded-xl border-2 border-black">
           <h2 className="text-xl font-semibold text-center">Habilidades Blandas</h2>
           <br></br>
           <ul className="list-disc list-inside">
---
+++ app/ui/contact.tsx
@@ -0,0 +1,12 @@
+export default function ContactMe() {
+  return (
+    <div className="flex max-lg:flex-col border w-full">
+      <div className="border w-full lg:w-2/4 h-auto">
+        hola
+      </div>
+      <div className="border w-full lg:w-2/4 h-auto">
+        mundo
+      </div>
+    </div>
+  )
+}
